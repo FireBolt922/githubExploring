180d15b0176d18ea8194e05b1ec3000eced38f9a
Ganesh <ganesh>
2023-08-24 16:45:12 +0530

    Adding Backend Test Cases

---
+++ backend/test_app/tests.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+
+
+class SampleTest(TestCase):
+
+    def test_1(self):
+        self.assertEqual(True, True)
+
+    
+    def test_2(self):
+        self.assertEqual(True, True)
